8c489bf6b8f1a682d3d35c66ac2d2afc5fd1bc13
Andy Cohan <andy@cohan.org>
2022-01-23 18:31:58 -0500

    working against some nasa dataset

--- app.py
+++ app.py
@@ -29,17 +29,32 @@ DATA_URL = 'https://www.nasa.gov/digitalstrategy/bureaudirectory.json'
 
 @app.route('/data')
 def data():
-    app.logger.info(f'downloading from {DATA_URL}')
-    download = requests.get(DATA_URL)
-    content = json.loads(download.content.decode('utf-8'))
 
     headers = ['name', 'imageUrl', 'area', 'profileUrl', 'office', 'tags', 'isLoggedUser', 'positionName', 'id', 'parentId', 'size']
     csvResult = StringIO('')
     writer = csv.writer(csvResult)
+    writer.writerow(headers)
+
+    app.logger.info(f'downloading from {DATA_URL}')
+    download = requests.get(DATA_URL)
+    content = json.loads(download.content.decode('utf-8'))
+
     for row in content['leaders']:
-        person = [f"{row['firstName']} {row['lastName']}"]
-        app.logger.debug(f'person: {person}')
+        person = [
+            f"{row['firstName']} {row['lastName']}", # name
+            f"", # imageurl
+            f"{row['employmentType']}", # area
+            f"", # profileurl
+            f"", # office
+            f"", # tags
+            f"", # isLoggedUser
+            f"{row['evaluationRatingOfficialTitle']}", # positionName
+            f"", # id
+            f"", # parentId
+            f"", # size
+        ]
         writer.writerow(person)
+
     output = make_response(csvResult.getvalue())
     output.headers["Content-type"] = "text/csv"
     output.headers["Content-Disposition"] = "attachment; filename=people.csv"
